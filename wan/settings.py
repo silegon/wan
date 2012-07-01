6d9f6b2b64c7450a8984d2df01e537006bc5ec29
silegon <silegon@gmail.com>
2012-07-01 17:13:03 +0800

    fix

--- wan/settings.py
+++ wan/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'reservation',                      # Or path to database file if using sqlite3.
+        'NAME': 'wan',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'root',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
